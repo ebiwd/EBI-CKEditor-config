f22768c6e70f18cb60ffb63231b83ec98c0a5b66
khawkins98 <khawkins98@gmail.com>
2018-02-15 11:03:19 +0000

    Add more styles

--- styles.js
+++ styles.js
@@ -6,12 +6,15 @@ Use this in your ckeditor config like:
   config.stylesSet = 'ebi_styles:https://ebiwd.github.io/EBI-CKEditor-config/styles.js';
 **/
 CKEDITOR.stylesSet.add("ebi_styles", [
-  { name: 'Sub head' , element: 'h3', attributes: { 'class': 'my_style' } },
+  { name: 'H3: Section head' , element: 'h3', attributes: { 'class': 'my_style' } },
+  { name: 'H4: Sub head' , element: 'h4', attributes: { 'class': 'my_style' } },
   { name: 'Lead paragraph' , element: 'p', attributes: { 'class': 'lead' } },
-  { name: 'CSS Style', element: 'span', attributes: { 'class': 'my_style' } },
-  { name: 'Marker: Yellow', element: 'span', styles: { 'background-color': 'Yellow' } },
+  { name: 'Small text' , element: 'span', attributes: { 'class': 'small' } },
+  { name: 'No underline', element: 'a', attributes: { 'class': 'no-underline' } },
   { name: 'Readmore', element: 'a', attributes: { 'class': 'readmore' } },
   { name: 'External', element: 'a', attributes: { 'class': 'external' } },
   { name: 'Button: Readmore', element: 'a', attributes: { 'class': 'button readmore' } },
-  { name: 'Button: External', element: 'a', attributes: { 'class': 'button external' } }
+  { name: 'Button: External', element: 'a', attributes: { 'class': 'button external' } },
+  { name: 'Button, secondary: Readmore', element: 'a', attributes: { 'class': 'button secondary readmore' } },
+  { name: 'Button, secondary: External', element: 'a', attributes: { 'class': 'button secondary external' } }
 ]);
