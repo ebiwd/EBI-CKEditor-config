9704543eb499fb2920d49cea2fec2dc1dffb91c3
khawkins98 <khawkins98@gmail.com>
2018-02-15 11:12:32 +0000

    Tweak

--- templates/ebi_templates.js
+++ templates/ebi_templates.js
@@ -3,9 +3,9 @@ CKEDITOR.addTemplates("ebi_templates",
   imagesPath:"https://ebiwd.github.io/EBI-CKEditor-config/templates/images/",
   templates:[
     {
-      title:"Image and Title",
+      title:"EBI Image and Title",
       image:"template1.gif",
-      description:"One main image with a title and text that surround the image.",
+      description:"EBI One main image with a title and text that surround the image.",
       html:'\x3ch3\x3e\x3cimg src\x3d" " alt\x3d"" style\x3d"margin-right: 10px" height\x3d"100" width\x3d"100" align\x3d"left" /\x3eType the title here\x3c/h3\x3e\x3cp\x3eType the text here\x3c/p\x3e'
     },
     {
