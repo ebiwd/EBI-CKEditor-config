947a4f5d3ed7678507cd75073728e380e55f4388
khawkins98 <khawkins98@gmail.com>
2018-02-15 10:50:46 +0000

    Initial stub for styles.js

---
+++ styles.js
@@ -0,0 +1,6 @@
+CKEDITOR.stylesSet.add("ebi_styles", [
+  { name: 'Sub head' , element: 'h3', attributes: { 'class': 'my_style' } },
+  { name: 'Lead paragraph' , element: 'p', attributes: { 'class': 'lead' } },
+  { name: 'CSS Style', element: 'span', attributes: { 'class': 'my_style' } },
+  { name: 'Marker: Yellow', element: 'span', styles: { 'background-color': 'Yellow' } }
+]);
