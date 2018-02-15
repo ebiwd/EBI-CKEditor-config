0dd9b269f7c5a68df35079a940f778ca8ef9d39d
khawkins98 <khawkins98@gmail.com>
2018-02-15 10:53:44 +0000

    Add more styles

--- styles.js
+++ styles.js
@@ -2,5 +2,9 @@ CKEDITOR.stylesSet.add("ebi_styles", [
   { name: 'Sub head' , element: 'h3', attributes: { 'class': 'my_style' } },
   { name: 'Lead paragraph' , element: 'p', attributes: { 'class': 'lead' } },
   { name: 'CSS Style', element: 'span', attributes: { 'class': 'my_style' } },
-  { name: 'Marker: Yellow', element: 'span', styles: { 'background-color': 'Yellow' } }
+  { name: 'Marker: Yellow', element: 'span', styles: { 'background-color': 'Yellow' } },
+  { name: 'Readmore', element: 'a', attributes: { 'class': 'readmore' } },
+  { name: 'External', element: 'a', attributes: { 'class': 'external' } },
+  { name: 'Button: Readmore', element: 'a', attributes: { 'class': 'button readmore' } },
+  { name: 'Button: External', element: 'a', attributes: { 'class': 'button external' } }
 ]);
