452f800d3a745bd6b0b3d7bd59b0ad8cb01807b5
khawkins98 <khawkins98@gmail.com>
2018-04-24 16:55:36 +0100

    Add accordion template

--- templates/ebi_templates.js
+++ templates/ebi_templates.js
@@ -45,6 +45,50 @@ CKEDITOR.addTemplates("ebi_templates",
               I'm being quoted here and want to be noticed. 
               <cite class="text-right">Hi, I'm a <a href="#">citation</a>!</cite>
             </blockquote>`
-    }
+    },
+    {
+      title:"Accordions",
+      image:"template1.gif",
+      description:"A basic set of accoridons.",
+      html:`<p>&nbsp;</p>
+
+            <div class="accordion-list">
+            <div aria-multiselectable="true" class="accordion-list-group accordion-list-items" id="accordion1524573360813" role="tablist">
+            <p>&nbsp;</p>
+
+            <div class="panel panel-default collapsible-item" id="Collapsible1524573407713">
+            <div class="panel-heading collapsible-item-heading" id="headingCollapsible1524573407713" role="tab">
+            <h4 class="panel-title collapsible-item-title"><a aria-controls="collapseCollapsible1524573407713" aria-expanded="false" class="collapsed collapsible-item-title-link-icon pull-right" data-parent="#accordion1524573360813" data-toggle="collapse" href="#collapseCollapsible1524573407713" role="button"><span class="glyphicon glyphicon-chevron-down">&nbsp;</span></a> <a aria-controls="collapseCollapsible1524573407713" aria-expanded="false" class="collapsed collapsible-item-title-link" data-parent="#accordion1524573360813" data-toggle="collapse" href="#collapseCollapsible1524573407713" role="button"><span>Title Text</span></a></h4>
+            </div>
+
+            <div aria-labelledby="headingCollapsible1524573407713" class="panel-collapse collapse collapsible-item-collapse" id="collapseCollapsible1524573407713" role="tabpanel">
+            <div class="panel-body collapsible-item-body">
+            <p>Body Text</p>
+            </div>
+            </div>
+            </div>
+
+            <p>&nbsp;</p>
+
+            <div class="collapsible-item panel panel-default" id="Collapsible1524573397896">
+            <div class="collapsible-item-heading panel-heading" id="headingCollapsible1524573397896" role="tab">
+            <h4 class="collapsible-item-title panel-title"><a aria-controls="collapseCollapsible1524573397896" aria-expanded="false" class="collapsed collapsible-item-title-link-icon" data-parent="#accordion1524573360813" data-toggle="collapse" href="#collapseCollapsible1524573397896" role="button"><span class="glyphicon glyphicon-chevron-down">&nbsp;</span></a> <a aria-controls="collapseCollapsible1524573397896" aria-expanded="false" class="collapsed collapsible-item-title-link" data-parent="#accordion1524573360813" data-toggle="collapse" href="#collapseCollapsible1524573397896" role="button">Title Text</a></h4>
+            </div>
+
+            <div aria-labelledby="headingCollapsible1524573397896" class="collapse collapsible-item-collapse panel-collapse" id="collapseCollapsible1524573397896" role="tabpanel">
+            <div class="collapsible-item-body panel-body">
+            <p>Body Text</p>
+            </div>
+            </div>
+            </div>
+            </div>
+            </div>
+
+            <p>&nbsp;</p>`
+    },
+
   ]
 });
+
+
+
